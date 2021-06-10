e37abf48ac5809302357e114922c70e908badc90
AlexBalew <balew48@gmail.com>
2021-06-10 15:56:04 +0300

    12 lesson components

---
+++ projectx/src/Components/Profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://www.imagesystems.com.mt/wp-content/uploads/2019/10/Who-we-are-Image-1-1-768x657.png' ></img>
+        </div>
+        <div>
+            ava+description
+    </div>
+        <div>
+            my posts
+      <div>
+                New post
+      </div>
+            <div>
+                post 1
+      </div>
+            <div>
+                post 2
+      </div>
+        </div>
+    </div>
+};
+
+
+export default Profile;
